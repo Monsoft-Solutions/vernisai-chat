2a90cad666cb4131494c261af550cdece9e40e4b
Adriano Flechilla <adrianof.dev@gmail.com>
2025-03-24 20:02:13 -0400

    feat(config): refine TypeScript and ESLint configurations for improved build process

---
+++ packages/config/.eslintrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ["@vernisai/eslint-config/index.js"],
+  ignorePatterns: ["dist/**", "node_modules/**", ".eslintrc.cjs"],
+  parserOptions: {
+    project: true,
+  },
+};
